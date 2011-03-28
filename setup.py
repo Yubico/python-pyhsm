b8bd8783ffa6c6853eed804be2bc3d843830439e
Fredrik Thulin <fredrik@yubico.com>
2011-03-28 15:15:25 +0200

    Version 0.9.2pre1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append('Tests');
 
 setup(name		= 'pyhsm',
-      version		= '0.9.0',
+      version		= '0.9.2pre1',
       description	= 'Python code for talking to a YubiHSM',
       author		= 'Fredrik Thulin',
       author_email	= 'fredrik@yubico.com',
