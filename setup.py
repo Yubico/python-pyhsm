e376aa8d30288eddd77312959ea2847982acc8dd
Fredrik Thulin <fredrik@yubico.com>
2011-05-04 17:04:21 +0200

    Forgot to update version for 0.9.8b.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append('Tests');
 
 setup(name		= 'pyhsm',
-      version		= '0.9.8',
+      version		= '0.9.8b',
       description	= 'Python code for talking to a YubiHSM',
       author		= 'Fredrik Thulin',
       author_email	= 'fredrik@yubico.com',
