c76ca6cdd078d290117db46ddeea41332bd90842
Simon Josefsson <simon@josefsson.org>
2012-09-12 10:11:43 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append('Tests');
 
 setup(name		= 'pyhsm',
-      version		= '1.0.4a',
+      version		= '1.0.4b',
       description	= 'Python code for talking to a YubiHSM',
       author		= 'Fredrik Thulin',
       author_email	= 'fredrik@yubico.com',
