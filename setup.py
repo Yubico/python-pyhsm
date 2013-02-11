166dc1a7a52a41561407f3f1666c0b36c111a40d
Dain Nilsson <dain@yubico.com>
2013-02-11 17:02:55 +0100

    Bumped version numbers post release.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import sys
 sys.path.append('Tests');
 
 setup(name		= 'pyhsm',
-      version		= '1.0.4b',
+      version		= '1.0.4c',
       description	= 'Python code for talking to a YubiHSM',
       author		= 'Fredrik Thulin',
       author_email	= 'fredrik@yubico.com',
