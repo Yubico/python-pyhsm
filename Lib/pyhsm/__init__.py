4eef59daee6a884bb01176274fdb58b3f2da6f4f
Klas Lindfors <klas@yubico.com>
2013-03-18 14:10:57 +0100

    bump versions post-release

--- Lib/pyhsm/__init__.py
+++ Lib/pyhsm/__init__.py
@@ -41,7 +41,7 @@ Basic usage ::
 See help(pyhsm.base) (L{pyhsm.base.YHSM}) for more information.
 """
 
-__version__ = '1.0.4c'
+__version__ = '1.0.4d'
 __copyright__ = 'Yubico AB'
 __organization__ = 'Yubico'
 __license__ = 'BSD'
