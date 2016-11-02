95ebe7cca2915ea63dd432860732f3b6e223d263
Dain Nilsson <dain@yubico.com>
2016-11-02 11:59:07 +0100

    Fix soft HSM keys validation.

--- pyhsm/soft_hsm.py
+++ pyhsm/soft_hsm.py
@@ -148,11 +148,11 @@ class SoftYHSM(object):
     def __init__(self, keys, debug=False):
         self._buffer = ''
         self.debug = debug
+        if not keys:
+            raise ValueError('Data contains no key handles!')
         for k, v in keys.items():
             if len(v) not in AES.key_size:
                 raise ValueError('Keyhandle of unsupported length: %d (was %d bytes)' % (k, len(v)))
-        else:
-            raise ValueError('Data contains no key handles!')
         self.keys = keys
 
     @classmethod
